b7bf1d4035a687155e7f4cef6b29e1d4eef78e5a
Jordan Hart <jordan.da.hart@gmail.com>
2022-06-28 19:51:16 +0100

    Fixed dyad ordering issue with conjugate models.

--- R/conjugate_models.R
+++ R/conjugate_models.R
@@ -10,36 +10,40 @@ fit_conjugate_model <- function(data_type, model_data, num_samples=10000) {
 
 fit_conjugate_model_binary <- function(model_data, num_samples) {
   # Draw samples from beta posterior
-  chain <- sapply(model_data$dyad_ids, function(i) {
-    rbeta(
+  chain <- matrix(0, num_samples, model_data$num_rows)
+  for (i in 1:model_data$num_rows) {
+    chain[, model_data$dyad_ids[i]] <- rbeta(
       num_samples,
       model_data$prior_edge_alpha + model_data$event[i],
       model_data$prior_edge_beta + model_data$divisor[i] - model_data$event[i]
     )
-  })
-  colnames(chain) <- model_data$dyad_ids
+  }
 
   # Draw predictions from binomial distribution
   event_preds <- t(sapply(1:num_samples, function(i) {
     rbinom(model_data$num_edges, model_data$divisor, chain[i, ])
   }))
+
+  chain <- qlogis(chain)
   return(list(chain=chain, event_preds=event_preds))
 }
 
 fit_conjugate_model_count <- function(model_data, num_samples) {
   # Draw samples from gamma posterior
-  chain <- sapply(model_data$dyad_ids, function(i) {
-    rgamma(
+  chain <- matrix(0, num_samples, model_data$num_rows)
+  for (i in 1:model_data$num_rows) {
+    chain[, model_data$dyad_ids[i]] <- rgamma(
       num_samples,
       model_data$prior_edge_alpha + model_data$event[i],
       model_data$prior_edge_beta/model_data$divisor[i] + 1
     )/model_data$divisor[i]
-  })
-  colnames(chain) <- model_data$dyad_ids
+  }
 
   # Draw predictions from Poisson distribution
   event_preds <- t(sapply(1:num_samples, function(i) {
     rpois(model_data$num_edges, chain[i, ] * model_data$divisor)
   }))
+
+  chain <- log(chain)
   return(list(chain=chain, event_preds=event_preds))
 }
